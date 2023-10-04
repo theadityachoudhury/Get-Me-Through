167a34c3b89365bb21fe88ba2a18d9d12b8272d9
Adwaith PJ <adwaithleans616@gmail.com>
2023-10-04 16:13:35 +0530

    update

--- FaceRecognition/Main_ignore.py
+++
@@ -1,9 +0,0 @@
-# import time
-
-
-# Set the timeout period in seconds
-# timeout_seconds = 10
-
-# Get the starting time
-# start_time = time.time()
-# and time.time() - start_time < timeout_seconds:
