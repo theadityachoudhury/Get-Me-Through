b2b4c317710ee6dbe9c40423ebdc2fded88a9884
Adwaith PJ <adwaithleans616@gmail.com>
2023-10-10 23:34:22 +0530

    Added Section and Branch

--- FaceRecognition/AddDataToDatabase.py
+++ FaceRecognition/AddDataToDatabase.py
@@ -1,5 +1,54 @@
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import db
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred,{
+    'databaseURL' : " https://faceattendancerealtime-a8bc3-default-rtdb.firebaseio.com/"
+})
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+
+ref = db.reference('Students')
+
+data = {
+    "2129013":
+        {
+            "name" : "Adwaith PJ",
+            "major" : "Computer science",
+            "section" : "CSCE",
+"branch" : "BTech",
+            "starting_year" : 2021,
+            "total_attendance" : 11,
+            "standing" : "G",
+            "year" : 4,
+            "last attendance_time " : "2022-12-11 00:54:34"
+        },"2129011":
+        {
+            "name" : "Aditya Choudhury",
+            "major" : "Computer science",
+            "section" : "CSCE",
+            "branch" : "BTech",
+            "starting_year" : 2021,
+            "total_attendance" : 10,
+            "standing" : "G",
+            "year" : 4,
+            "last attendance_time " : "2022-12-11 00:54:34"
+        },"2129010":
+        {
+            "name" : "Aditya Singh",
+            "major" : "Computer science",
+            "section" : "CSCE",
+"branch" : "BTech",
+            "starting_year" : 2021,
+            "total_attendance" : 7,
+            "standing" : "G",
+            "year" : 4,
+            "last attendance_time " : "2022-12-11 00:54:34"
+        },
+
+
+
+}
+
+
+for key,value in data.items():
+    ref.child(key).set(value)
