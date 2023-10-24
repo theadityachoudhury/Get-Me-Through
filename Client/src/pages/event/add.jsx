5bd24c63166c602ce91e6a32c9ff4bd6cc2ceaaa
Aditya Choudhury <adityasubham03@gmail.com>
2023-10-24 20:14:30 +0530

    Updated events page and added functionality of all buttons and updated the form

--- Client/src/pages/event/add.jsx
+++ Client/src/pages/event/add.jsx
@@ -1,139 +1,356 @@
-import { useState, useRef, useEffect } from "react";
-import * as faceapi from "face-api.js";
-import { Link } from "react-router-dom";
+import { useState, useContext } from "react";
+import { UserContext } from "../../UserContext";
+import { Link, Navigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const EventAddForm = () => {
-    const [formData, setFormData] = useState({
-        "eventName": "",
-        "eventDescription": "",
-        "eventHost": "",
-        "eventDate": "2023-04-01",
-        "maxParticipation": 100
-    });
+	const { ready, user } = useContext(UserContext);
+	const initialFormData = {
+		title: "",
+		description: "",
+		date: formatDateToDDMMYYYY(new Date()),
+		location: "",
+		organizer: "",
+		category: "",
+		registrationFee: 0,
+		capacity: 100,
+		registrationDeadline: formatDateToDDMMYYYY(new Date()),
+		status: "upcoming",
+		tags: [],
+		tagsInput: "",
+	};
+	const resetFormData = () => {
+		setFormData(initialFormData);
+	};
 
+	// Define initial formData with the required fields as per the schema
+	const [formData, setFormData] = useState({
+		title: "",
+		description: "",
+		date: formatDateToDDMMYYYY(new Date()),
+		location: "",
+		organizer: "",
+		category: "",
+		registrationFee: 0,
+		capacity: 100,
+		registrationDeadline: formatDateToDDMMYYYY(new Date()),
+		status: "upcoming",
+		tags: [],
+		tagsInput: "", // Added tagsInput state
+	});
 
-    function formatDateToDDMMYYYY(date) {
-        const day = date.getDate().toString().padStart(2, '0'); // Get day and pad with leading zero if needed
-        const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Get month (adding 1 as it's zero-based) and pad with leading zero if needed
-        const year = date.getFullYear().toString(); // Get full year
-        return `${year}-${month}-${day}`;
-      }
+	function formatDateToDDMMYYYY(date) {
+		const day = date.getDate().toString().padStart(2, "0");
+		const month = (date.getMonth() + 1).toString().padStart(2, "0");
+		const year = date.getFullYear().toString();
+		return `${year}-${month}-${day}`;
+	}
 
-    const handleChange = (e,name) => {
-        const { value } = e.target;
-        setFormData({
-          ...formData,
-          [name]: value,
-        });
-    };
-    
-    const handleDate = (e) => {
-        console.log(e.target.value);
-        setFormData({
-            ...formData,
-            "eventDate": formatDateToDDMMYYYY(new Date(e.target.value))
-        })
-    }
+	const handleChange = (e, name) => {
+		const { value } = e.target;
+		setFormData({
+			...formData,
+			[name]: value,
+		});
+	};
 
+	const handleDate = (e) => {
+		setFormData({
+			...formData,
+			[e.target.name]: e.target.value,
+		});
+	};
 
-	const handleSubmit = (e) => {
+	const handleTagsChange = (e) => {
+		// Add tags to the array when Enter key is pressed
+		if (e.key === "Enter" && e.target.value.trim() !== "") {
+			e.preventDefault();
+			e.stopPropagation();
+			if (formData.tags.length < 5) {
+				setFormData({
+					...formData,
+					tags: [...formData.tags, e.target.value.trim()],
+					tagsInput: "",
+				});
+				e.target.value = "";
+			}
+		}
+	};
+
+	const removeTag = (tagToRemove) => {
+		setFormData({
+			...formData,
+			tags: formData.tags.filter((tag) => tag !== tagToRemove),
+		});
+	};
+
+	const handleSubmit = async (e) => {
 		e.preventDefault();
 		console.log("clicked");
 		// Handle form submission
 		// You can add your registration logic here
+		const eventData = {
+			title: formData.title,
+			description: formData.description,
+			date: formData.date,
+			location: formData.location,
+			organizer: formData.organizer,
+			category: formData.category,
+			registrationFee: formData.registrationFee,
+			capacity: formData.capacity,
+			registrationDeadline: formData.registrationDeadline,
+			status: formData.status,
+			tags: formData.tags,
+		};
+
+		const config = {
+			method: "post",
+			maxBodyLength: Infinity,
+			url: "/api/events/add",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			data: eventData,
+		};
+
+		try {
+			const response = await axios.request(config);
+			toast.success("Success: " + response.data.message);
+			resetFormData();
+		} catch (error) {
+			console.error(error);
+			const errorData = error.response
+				? error.response.data.message
+				: "An unexpected error occurred!!";
+			toast.error(errorData);
+		}
 	};
 
-	return (
-		<div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
-			<div className="w-full max-w-md p-8 bg-white rounded shadow-md">
-				<h2 className="text-2xl font-bold mb-4 text-center">
-					Events Registration
-				</h2>
-				<form onSubmit={handleSubmit}>
-					<div className="mb-4">
-						<label htmlFor="eventName" className="block">
-							Event Name:
-						</label>
-						<input
-							type="text"
-							id="eventName"
-							value={formData["eventName"]}
-							onChange={(e) => handleChange(e,"eventName")}
-							className="border border-gray-300 rounded px-2 py-1"
-							required
-						/>
-					</div>
+	if (ready) {
+		if (user && user.role === "admin") {
+			return (
+				<div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
+					<div className="w-full max-w-md p-8 bg-white rounded shadow-md">
+						<h2 className="text-2xl font-bold mb-4 text-center">
+							Events Registration
+						</h2>
+						<form onSubmit={handleSubmit}>
+							<div className="mb-4">
+								<label htmlFor="title" className="block">
+									Event Name:
+								</label>
+								<input
+									type="text"
+									id="title"
+									name="title"
+									value={formData.title}
+									onChange={(e) => handleChange(e, "title")}
+									className="border border-gray-300 rounded px-2 py-1"
+									required
+								/>
+							</div>
 
-					<div className="mb-4">
-						<label htmlFor="eventDescription" className="block mb-2">
-							Event Description:
-						</label>
-						<textarea
-							type="text"
-							id="eventDescription"
-							value={formData["eventDescription"]}
-							onChange={(e) => handleChange(e,"eventDescription")}
-                            className="border border-gray-300 rounded px-2 py-1"
-                            rows={10}
-							required
-						/>
-                    </div>
-                    
-                    <div className="mb-4">
-						<label htmlFor="eventName" className="block">
-							Event Host:
-						</label>
-						<input
-							type="text"
-							id="eventName"
-							value={formData["eventHost"]}
-							onChange={(e) => handleChange(e,"eventHost")}
-							className="border border-gray-300 rounded px-2 py-1"
-							required
-						/>
-                    </div>
-                    
-                    <div className="mb-4">
-						<label htmlFor="eventDate" className="block">
-							Event Date:
-						</label>
-						<input
-							type="date"
-							id="eventDate"
-							value={formData["eventDate"]}
-							onChange={handleDate}
-							className="border border-gray-300 rounded px-2 py-1"
-							required
-						/>
-                    </div>
-                    
-                    <div className="mb-4">
-						<label htmlFor="maxParticipation" className="block">
-							Max Participation:
-						</label>
-						<input
-							type="number"
-							id="maxParticipation"
-							value={formData["maxParticipation"]}
-							onChange={(e) => handleChange(e,"maxParticipation")}
-                            className="border border-gray-300 rounded px-2 py-1"
-                            min={100}
-							required
-						/>
-					</div>
+							<div className="mb-4">
+								<label htmlFor="description" className="block mb-2">
+									Event Description:
+								</label>
+								<textarea
+									id="description"
+									name="description"
+									value={formData.description}
+									onChange={(e) => handleChange(e, "description")}
+									className="border border-gray-300 rounded px-2 py-1"
+									rows={3}
+									required
+								/>
+							</div>
 
-				
-					<button
-						type="submit"
-						className="bg-green-500 text-white px-4 py-2 rounded">
-						Register
-					</button>
-					
-                </form>
-                <p className="mt-3 text-center text-red">Don't have an account yet? <Link to="/register">Register Here</Link></p>
-			</div>
-		</div>
-	);
+							<div className="mb-4">
+								<label htmlFor="location" className="block">
+									Location:
+								</label>
+								<input
+									type="text"
+									id="location"
+									name="location"
+									value={formData.location}
+									onChange={(e) => handleChange(e, "location")}
+									className="border border-gray-300 rounded px-2 py-1"
+									required
+								/>
+							</div>
+
+							<div className="mb-4">
+								<label htmlFor="organizer" className="block">
+									Event Host:
+								</label>
+								<input
+									type="text"
+									id="organizer"
+									name="organizer"
+									value={formData.organizer}
+									onChange={(e) => handleChange(e, "organizer")}
+									className="border border-gray-300 rounded px-2 py-1"
+									required
+								/>
+							</div>
+
+							<div className="mb-4">
+								<label htmlFor="date" className="block">
+									Event Date:
+								</label>
+								<input
+									type="date"
+									id="date"
+									name="date"
+									value={formData.date}
+									onChange={handleDate}
+									className="border border-gray-300 rounded px-2 py-1"
+									required
+								/>
+							</div>
+
+							<div className="mb-4">
+								<label htmlFor="category" className="block">
+									Category:
+								</label>
+								<input
+									type="text"
+									id="category"
+									name="category"
+									value={formData.category}
+									onChange={(e) => handleChange(e, "category")}
+									className="border border-gray-300 rounded px-2 py-1"
+									required
+								/>
+							</div>
+
+							<div className="mb-4">
+								<label htmlFor="registrationFee" className="block">
+									Registration Fee:
+								</label>
+								<input
+									type="number"
+									id="registrationFee"
+									name="registrationFee"
+									value={formData.registrationFee}
+									onChange={(e) => handleChange(e, "registrationFee")}
+									className="border border-gray-300 rounded px-2 py-1"
+								/>
+							</div>
+
+							<div className="mb-4">
+								<label htmlFor="capacity" className="block">
+									Max Participation:
+								</label>
+								<input
+									type="number"
+									id="capacity"
+									name="capacity"
+									value={formData.capacity}
+									onChange={(e) => handleChange(e, "capacity")}
+									className="border border-gray-300 rounded px-2 py-1"
+									min={100}
+									required
+								/>
+							</div>
+
+							<div className="mb-4">
+								<label htmlFor="registrationDeadline" className="block">
+									Registration Deadline:
+								</label>
+								<input
+									type="date"
+									id="registrationDeadline"
+									name="registrationDeadline"
+									value={formData.registrationDeadline}
+									onChange={handleDate}
+									className="border border-gray-300 rounded px-2 py-1"
+									required
+								/>
+							</div>
+
+							<div className="mb-4">
+								<label htmlFor="status" className="block">
+									Event Status:
+								</label>
+								<select
+									id="status"
+									name="status"
+									value={formData.status}
+									onChange={(e) => handleChange(e, "status")}
+									className="border border-gray-300 rounded px-2 py-1">
+									<option value="upcoming">Upcoming</option>
+									<option value="past">Past</option>
+									<option value="canceled">Canceled</option>
+								</select>
+							</div>
+
+							<div className="mb-4">
+								<label htmlFor="tags" className="block mb-2">
+									Event Tags:
+								</label>
+								{formData.tags.length < 5 ? (
+									<input
+										type="text"
+										id="tags"
+										name="tags"
+										value={formData.tagsInput}
+										onKeyPress={handleTagsChange}
+										onChange={(e) =>
+											setFormData({ ...formData, tagsInput: e.target.value })
+										}
+										className="border border-gray-300 rounded px-2 py-1"
+										placeholder={`You can add ${5 - formData.tags.length} tags`}
+									/>
+								) : (
+									<input
+										type="text"
+										id="tags"
+										name="tags"
+										value={formData.tagsInput}
+										onKeyPress={handleTagsChange}
+										onChange={(e) =>
+											setFormData({ ...formData, tagsInput: e.target.value })
+										}
+										className="border border-gray-300 rounded px-2 py-1"
+										placeholder="Upto 5 tags only. Remove to add more"
+										disabled
+									/>
+								)}
+								<div className="flex flex-wrap mt-2">
+									{formData.tags.map((tag) => (
+										<div
+											key={tag}
+											className="bg-blue-100 text-blue-800 rounded-full px-2 py-1 m-1 flex items-center">
+											<span style={{ marginLeft: "15px" }} className="">
+												{tag}
+											</span>
+											<button
+												type="button"
+												onClick={() => removeTag(tag)}
+												className="text-red-600 text-sm" // Reduced font size for the cross button
+											>
+												&#10005;
+											</button>
+										</div>
+									))}
+								</div>
+							</div>
+
+							<button
+								className="bg-green-500 text-white px-4 py-2 rounded">
+								Register
+							</button>
+						</form>
+					</div>
+				</div>
+			);
+		} else {
+			return <Navigate to={"/events"} />;
+		}
+	}
 };
 
 export default EventAddForm;
--- Client/src/pages/event/event.jsx
+++ Client/src/pages/event/event.jsx
@@ -1,97 +1,198 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios"; // You may need to install axios if not already installed
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import { UserContext } from "../../UserContext";
+import { Link } from "react-router-dom";
+import EventModalView from "../components/EventsModals/EventModalView";
+import EventModalEdit from "../components/EventsModals/EventModalEdit";
+import toast from "react-hot-toast";
 
 const EventPage = () => {
-  const [events, setEvents] = useState([]);
+	const { ready, user } = useContext(UserContext);
+	const [events, setEvents] = useState([]);
+	const [selectedEventForView, setSelectedEventForView] = useState(null);
+	const [selectedEventForEdit, setSelectedEventForEdit] = useState(null);
 
-  useEffect(() => {
-    axios
-      .get("API_URL_HERE")
-      .then((response) => {
-          const eventData = response.data;
-          setEvents(eventData);
-        //   console.log(events);
-      })
-      .catch((error) => {
-        console.error("Error fetching event data:", error);
-      });
+	const handleViewClick = async (event) => {
+		const { data } = await axios.get(`/api/events/${event._id}`);
+		setSelectedEventForView(data);
+	};
 
-      const eventData = [
-        {
-          "eventName": "Event 1",
-          "eventDescription": "Description for Event 1",
-          "eventHost": "Host 1",
-          "eventDate": "2023-04-01",
-          "maxParticipation": 100
-        },
-        {
-          "eventName": "Event 2",
-          "eventDescription": "Description for Event 2",
-          "eventHost": "Host 2",
-          "eventDate": "2023-04-15",
-          "maxParticipation": 150
-        },  
-      ]
-      
-      setEvents(eventData);
-    //   console.log(events);
-  }, []);
+	const handleEditClick = async (event) => {
+		const { data } = await axios.get(`/api/events/${event._id}`);
+		setSelectedEventForEdit(data);
+	};
 
-  return (
-    <div className="event-page p-6 bg-gray-100">
-      <h1 className="text-2xl font-semibold mb-4">Events</h1>
-      <table className="min-w-full divide-y divide-gray-200">
-        <thead>
-          <tr>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Event Name
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Event Description
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Host
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Date
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Max Participation
-            </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              Actions
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {events.map((event, index) => (
-            <tr key={index} className="bg-white">
-              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                {event.eventName}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {event.eventDescription}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {event.eventHost}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {event.eventDate}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {event.maxParticipation}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                <button className="mr-2 text-black hover:underline p-1 w-16 h-8 bg-blue-200 hover:bg-blue-300">View</button>
-                <button className="mr-2 text-black hover:underline p-1 w-16 h-8 bg-green-200 hover:bg-green-300">Edit</button>
-                <button className="text-black hover:underline p-1 w-16 h-8 bg-red-200 hover:bg-red-300">Delete</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+	const handleCloseModal = () => {
+		setSelectedEventForView(null);
+		setSelectedEventForEdit(null);
+	};
+
+	useEffect(() => {
+		axios
+			.get("/api/events")
+			.then((response) => {
+				const eventData = response.data;
+				setEvents(eventData);
+			})
+			.catch((error) => {
+				console.error("Error fetching event data:", error);
+			});
+	}, []);
+
+	const handleDelete = (eventId) => {
+		axios
+			.delete(`/api/events/${eventId}`)
+			.then((response) => {
+				if (response.status === 200) {
+					setEvents((prevEvents) =>
+						prevEvents.filter((event) => event._id !== eventId)
+					);
+					toast.success("Event deleted successfully");
+					
+				}
+			})
+			.catch((error) => {
+				console.error("Error deleting event:", error);
+				toast.error("Unable to delete event!!");
+			});
+	};
+
+	const handleEscapeKey = (event) => {
+		if (event.key === "Escape") {
+			handleCloseModal();
+		}
+	};
+
+	useEffect(() => {
+		// Add event listener to listen for "Esc" key press
+		document.addEventListener("keydown", handleEscapeKey);
+
+		// Clean up the event listener when the component unmounts
+		return () => {
+			document.removeEventListener("keydown", handleEscapeKey);
+		};
+	}, []);
+
+	const handleEditSuccess = (editedEvent) => {
+		// Find the index of the edited event in the current events array
+		const editedIndex = events.findIndex(
+			(event) => event._id === editedEvent._id
+		);
+
+		if (editedIndex !== -1) {
+			// Clone the current events array to avoid mutating state directly
+			const updatedEvents = [...events];
+			// Update the event at the edited index with the edited data
+			updatedEvents[editedIndex] = editedEvent;
+			// Set the updated events array in state
+			setEvents(updatedEvents);
+		}
+
+		// Close the edit modal
+		setSelectedEventForEdit(null);
+	};
+
+	if (user && ready) {
+		return (
+			<div className="event-page p-6 bg-gray-100 mb-10">
+				<div className="flex items-center">
+					<h1 className="text-5xl font-semibold text-gray-800">EVENTS</h1>
+					{user.role === "admin" && (
+						<Link to="/events/add" className="ml-4">
+							<button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md">
+								Add Event
+							</button>
+						</Link>
+					)}
+				</div>
+
+				<table className="min-w-full divide-y divide-gray-200">
+					<thead>
+						<tr>
+							<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+								Event Name
+							</th>
+							<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+								Event Description
+							</th>
+							<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+								Host
+							</th>
+							<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+								Date
+							</th>
+							<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+								Max Participation
+							</th>
+							<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+								Actions
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{events.map((event, index) => (
+							<tr key={index} className="bg-white">
+								<td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+									{event.title}
+								</td>
+								<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+									{event.description}
+								</td>
+								<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+									{event.organizer}
+								</td>
+								<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+									{new Intl.DateTimeFormat("en-US", {
+										dateStyle: "short",
+									}).format(new Date(event.date))}
+								</td>
+								<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+									{event.capacity}
+								</td>
+								<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+									<button
+										className="mr-2 text-black p-1 w-16 h-8 bg-blue-200 hover:bg-blue-300"
+										onClick={() => handleViewClick(event)}>
+										View
+									</button>
+
+									{user.role === "admin" && (
+										<>
+											<button
+												className="mr-2 text-black p-1 w-16 h-8 bg-green-200 hover:bg-green-300"
+												onClick={() => handleEditClick(event)}>
+												Edit
+											</button>
+											<button
+												className="text-black p-1 w-16 h-8 bg-red-200 hover.bg-red-300"
+												onClick={() => handleDelete(event._id)}>
+												Delete
+											</button>
+										</>
+									)}
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+				{selectedEventForView && (
+					<EventModalView
+						event={selectedEventForView}
+						isOpen={selectedEventForView !== null}
+						onClose={handleCloseModal}
+					/>
+				)}
+				{selectedEventForEdit && (
+					<EventModalEdit
+						event={selectedEventForEdit}
+						isOpen={selectedEventForEdit !== null}
+						onClose={handleCloseModal}
+						onEditSuccess={handleEditSuccess}
+					/>
+				)}
+			</div>
+		);
+	}
 };
 
 export default EventPage;
