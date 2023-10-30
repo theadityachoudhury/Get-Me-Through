e0a0ea981edaa2f98c7eafcd31d6b96bad0951ff
Aditya Choudhury <adityasubham03@gmail.com>
2023-10-30 15:20:25 +0530

    2

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,38 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// // export default defineConfig({
-// //   plugins: [react()],
-// // })
-
-// export default defineConfig({
-//   plugins: [react({
-//     // Add this line
-//     include: "**/*.jsx",
-//   })],
-//   server: {
-//     watch: {
-//       usePolling: true
-//     }
-//   },
-//   build: {
-//     chunkSizeWarningLimit: 2000, // Set to 1 MB
-//   },
-// })
-
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173, // You can change the port as needed
-  },
-  build: {
-    outDir: 'dist', // Output directory for production build
-  },
-});
+})
