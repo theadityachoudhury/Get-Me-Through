bd8ac2b2925096447f73d5d47d8d531f0d43aba7
Aditya Choudhury <adityasubham03@gmail.com>
2023-10-30 14:27:37 +0530

    Updated card component and completed dashboard component

--- Client/src/pages/components/Card.jsx
+++ Client/src/pages/components/Card.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Card = ({ title, description }) => {
-  return (
-    <div className="w-96 h-96 rounded-lg shadow-xxl bg-gray-300">
-      <div className="font-bold text-2xl mb-4">{title}</div>
-      <p className="text-gray-700 text-base">{description}</p>
-      <div className="mt-4">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Learn More
-        </button>
-      </div>
-    </div>
-  );
+const Card = ({ title, description, link }) => {
+	return (
+		<div className="w-96 h-96 rounded-lg shadow-xxl bg-gray-300 flex flex-col justify-center items-center ml-16 mb-6">
+			<div className="font-bold text-2xl mb-4 text-center">{title}</div>
+			<p className="text-gray-700 text-base text-center">{description}</p>
+			<div className="mt-4">
+				<Link to={link}>
+					<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+						Learn More
+					</button>
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default Card;
